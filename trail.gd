1f808c0620958732293cfb201802628a4f2d4892
HungryProton <exb2800+github@protonmail.com>
2019-06-23 16:25:20 +0200

    add: comments add: uv coords

--- trail.gd
+++ trail.gd
@@ -1,5 +1,36 @@
 extends Spatial
 
+# --
+# GM Trail
+# --
+# This node generates a 3D trail at runtime
+# Two nodes defines the width of the the trail. At each tick, the process
+# function update the trail geometry, add new points if the latest ones 
+# are too far away from the previous ones, and remove the oldest points 
+# if they expired.
+#
+# This behavior will probably change in the future to accomodate
+# bezier curves, camera facing and other features.
+# --
+# material : The material applied to the trail. UV coordinates are
+#   defined as follow :
+#   + The most recent points are at the left of the texture (0,0) and (0,1)
+#   + The oldest points are at the right (1,0) and (1,1)
+#   + That means the texture is stretch accross the entire trail at all times
+#
+# resolution : The maximum distance between two sets of points.
+#   + One caveat though : If the two trails drivers moved farther than the
+#     trail resolution in one frame, intermediate vertices will not be created
+# 
+# life_time : How long a point can exist in the trail.
+#
+# emit : True means the trail will keep creating new points. False means it
+#   will only draw points still in memory and delete them, but wont create
+#   new geometry.
+# --
+
+class_name GM_Trail
+
 export(Material) var material
 export(float) var resolution = 0.2
 export(float) var life_time = 0.1
@@ -32,9 +63,8 @@ func _ready():
 
 func _process(delta):
 	_update_midpoint()
-	if emit:
-		_update_last_points()
-		_update_geometry_data(delta)
+	_update_last_points()
+	_update_geometry_data(delta)
 	_draw_geometry()
 
 func _update_last_points():
@@ -50,11 +80,11 @@ func _update_geometry_data(delta):
 		data[i].ttl -= delta
 		if data[i].ttl <= 0:
 			data.pop_back()
-			
+	if not emit:
+		return
 	if len(data) <= 1:
 		_add_point_to_trail()
 		return
-
 	if data[0].midpoint.distance_squared_to(data[1].midpoint) >= resolution_squared:
 		_add_point_to_trail()
 
@@ -71,9 +101,14 @@ func _draw_geometry():
 		return
 	geometry.clear()
 	geometry.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP, null)
-
-	for i in range(len(data)):
+	var count = len(data)
+	for i in range(count):
+		var uv_x = i/(count+0.0)
+		
+		geometry.set_uv(Vector2(uv_x, 1.0))
 		geometry.add_vertex(data[i].p1)
+		
+		geometry.set_uv(Vector2(uv_x , 0.0))
 		geometry.add_vertex(data[i].p2)
 
 	geometry.end()
